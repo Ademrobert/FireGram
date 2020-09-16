4a5053e38af50d5015f8708b04aa0ad1f0507ba9
timppaB <adembenothman@gmail.com>
2020-09-16 21:04:30 +0300

    Added styling to imgaes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Title from './comps/Title';
 import UploadFrom from './comps/UploadForm';
+import ImageGrid from './comps/ImageGrid';
+import Model from './comps/Modal';
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadFrom />
+      <ImageGrid />
+      <Model />
     </div>
   );
 }
---
+++ src/comps/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Model = () => {
+    return(
+        <div className="backdrop">
+            <img src="" alt="enlarged pic" />
+        </div>
+    )
+}
+
+export default Model;
